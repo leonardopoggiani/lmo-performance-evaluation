68a12fb4c4d9d59445c97a45b9853bc3e309be64
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2023-08-14 10:51:43 +0200

    feat(pe): add performance evaluation e2e (client-server)

--- e2e/performance_evaluation.go
+++ e2e/performance_evaluation.go
@@ -438,6 +438,140 @@ func getCheckpointTimePipelined(ctx context.Context, clientset *kubernetes.Clien
 	cleanUp(ctx, clientset, pod)
 }
 
+func getRestoreTime(ctx context.Context, clientset *kubernetes.Clientset, numContainers int, db *sql.DB) {
+	// Get the start time of the restore
+
+	pod := CreateContainers(ctx, numContainers, clientset)
+
+	LiveMigrationReconciler := migrationoperator.LiveMigrationReconciler{}
+
+	err := LiveMigrationReconciler.WaitForContainerReady(pod.Name, "default", fmt.Sprintf("container-%d", numContainers-1), clientset)
+	if err != nil {
+		cleanUp(ctx, clientset, pod)
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Create a slice of Container structs
+	var containers []migrationoperator.Container
+
+	// Append the container ID and name for each container in each pod
+	pods, err := clientset.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		fmt.Println(err.Error())
+		cleanUp(ctx, clientset, pod)
+		return
+	}
+
+	for _, pod := range pods.Items {
+		for _, containerStatus := range pod.Status.ContainerStatuses {
+			idParts := strings.Split(containerStatus.ContainerID, "//")
+			if len(idParts) < 2 {
+				fmt.Println("Malformed container ID")
+				return
+			}
+			containerID := idParts[1]
+
+			container := migrationoperator.Container{
+				ID:   containerID,
+				Name: containerStatus.Name,
+			}
+			containers = append(containers, container)
+		}
+	}
+
+	err = LiveMigrationReconciler.CheckpointPodPipelined(containers, "default", pod.Name)
+	if err != nil {
+		return
+	}
+
+	path := "/tmp/checkpoints/checkpoints/"
+	// create dummy file
+	_, err = os.Create(path + "dummy")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	files, err := countFilesInFolder(path)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("Files count => %d", files)
+
+	start := time.Now()
+
+	pod, err = LiveMigrationReconciler.BuildahRestore(ctx, "/tmp/checkpoints/checkpoints", clientset)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	// Calculate the time taken for the restore
+	elapsed := time.Since(start)
+	fmt.Println("Elapsed sequential: ", elapsed)
+
+	saveToDB(db, int64(numContainers), float64(elapsed.Milliseconds()), "sequential", "restore_times")
+
+	// eliminate docker image
+	for i := 0; i < numContainers; i++ {
+		buildahDeleteImage("localhost/leonardopoggiani/checkpoint-images:container-" + strconv.Itoa(i))
+	}
+
+	cleanUp(ctx, clientset, pod)
+	start = time.Now()
+
+	pod, err = LiveMigrationReconciler.BuildahRestorePipelined(ctx, "/tmp/checkpoints/checkpoints", clientset)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	// Calculate the time taken for the restore
+	elapsed = time.Since(start)
+	fmt.Println("Elapsed pipelined: ", elapsed)
+
+	saveToDB(db, int64(numContainers), float64(elapsed.Milliseconds()), "pipelined", "restore_times")
+
+	// eliminate docker image
+	for i := 0; i < numContainers; i++ {
+		buildahDeleteImage("localhost/leonardopoggiani/checkpoint-images:container-" + strconv.Itoa(i))
+	}
+
+	cleanUp(ctx, clientset, pod)
+
+	start = time.Now()
+
+	pod, err = LiveMigrationReconciler.BuildahRestoreParallelized(ctx, "/tmp/checkpoints/checkpoints", clientset)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Calculate the time taken for the restore
+	elapsed = time.Since(start)
+	fmt.Println("Elapsed parallel: ", elapsed)
+
+	saveToDB(db, int64(numContainers), float64(elapsed.Milliseconds()), "parallelized", "restore_times")
+
+	// eliminate docker image
+	for i := 0; i < numContainers; i++ {
+		buildahDeleteImage("localhost/leonardopoggiani/checkpoint-images:container-" + strconv.Itoa(i))
+	}
+
+	cleanUp(ctx, clientset, pod)
+}
+
+func buildahDeleteImage(imageName string) {
+	buildahCmd := exec.Command("sudo", "buildah", "rmi", imageName)
+	err := buildahCmd.Run()
+	if err != nil {
+		fmt.Println("Error removing image:", err)
+		return
+	}
+	fmt.Println("Image", imageName, "removed successfully.")
+}
+
 func getImageSize(imageName string) (float64, error) {
 	// Build the command to get the image size
 	cmd := exec.Command("sudo", "buildah", "inspect", "-f", "{{.Size}}", imageName)
@@ -458,6 +592,198 @@ func getImageSize(imageName string) (float64, error) {
 	sizeInMB := sizeInBytes / (1024 * 1024)
 
 	return sizeInMB, nil
+}
+
+func getCheckpointImageRestoreSize(ctx context.Context, clientset *kubernetes.Clientset, numContainers int, db *sql.DB) {
+	pod := CreateContainers(ctx, numContainers, clientset)
+
+	LiveMigrationReconciler := migrationoperator.LiveMigrationReconciler{}
+
+	err := LiveMigrationReconciler.WaitForContainerReady(pod.Name, "default", fmt.Sprintf("container-%d", numContainers-1), clientset)
+	if err != nil {
+		cleanUp(ctx, clientset, pod)
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("Pod %s is ready\n", pod.Name)
+
+	// Create a slice of Container structs
+	var containers []migrationoperator.Container
+
+	// Append the container ID and name for each container in each pod
+	pods, err := clientset.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		fmt.Println(err.Error())
+		cleanUp(ctx, clientset, pod)
+		return
+	}
+
+	for _, pod := range pods.Items {
+		for _, containerStatus := range pod.Status.ContainerStatuses {
+			idParts := strings.Split(containerStatus.ContainerID, "//")
+			if len(idParts) < 2 {
+				fmt.Println("Malformed container ID")
+				return
+			}
+			containerID := idParts[1]
+
+			container := migrationoperator.Container{
+				ID:   containerID,
+				Name: containerStatus.Name,
+			}
+			containers = append(containers, container)
+		}
+	}
+
+	err = LiveMigrationReconciler.CheckpointPodCrio(containers, "default", pod.Name)
+	if err != nil {
+		fmt.Println(err.Error())
+		cleanUp(ctx, clientset, pod)
+		return
+	}
+
+	directory := "/tmp/checkpoints/checkpoints"
+
+	for i := 0; i < numContainers; i++ {
+		// Get the image name
+		imageName := "localhost/leonardopoggiani/checkpoint-images:container-" + strconv.Itoa(i)
+
+		// Get the image size
+		sizeInMB, err := getImageSize(imageName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("The size of %s is %.2f MB.\n", imageName, sizeInMB)
+	}
+
+	// delete checkpoints folder
+	if _, err := exec.Command("sudo", "rm", "-f", directory+"/").Output(); err != nil {
+		cleanUp(ctx, clientset, pod)
+		fmt.Println(err.Error())
+		return
+	}
+
+	// check that checkpoints folder is empty
+	if output, err := exec.Command("sudo", "ls", directory).Output(); err != nil {
+		cleanUp(ctx, clientset, pod)
+		fmt.Println(err.Error())
+		return
+	} else {
+		fmt.Printf("Output: %s\n", output)
+	}
+
+	cleanUp(ctx, clientset, pod)
+}
+
+func getTimeDirectVsTriangularized(ctx context.Context, clientset *kubernetes.Clientset, numContainers int, db *sql.DB, exchange string) {
+	pod := CreateContainers(ctx, numContainers, clientset)
+
+	LiveMigrationReconciler := migrationoperator.LiveMigrationReconciler{}
+
+	err := LiveMigrationReconciler.WaitForContainerReady(pod.Name, "default", fmt.Sprintf("container-%d", numContainers-1), clientset)
+	if err != nil {
+		cleanUp(ctx, clientset, pod)
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Create a slice of Container structs
+	var containers []migrationoperator.Container
+
+	// Append the container ID and name for each container in each pod
+	pods, err := clientset.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		fmt.Println(err.Error())
+		cleanUp(ctx, clientset, pod)
+		return
+	}
+
+	for _, pod := range pods.Items {
+		for _, containerStatus := range pod.Status.ContainerStatuses {
+			idParts := strings.Split(containerStatus.ContainerID, "//")
+			if len(idParts) < 2 {
+				fmt.Println("Malformed container ID")
+				return
+			}
+			containerID := idParts[1]
+
+			container := migrationoperator.Container{
+				ID:   containerID,
+				Name: containerStatus.Name,
+			}
+			containers = append(containers, container)
+		}
+	}
+
+	start := time.Now()
+
+	err = LiveMigrationReconciler.CheckpointPodCrio(containers, "default", pod.Name)
+	if err != nil {
+		fmt.Println(err.Error())
+		cleanUp(ctx, clientset, pod)
+		return
+	}
+
+	cleanUp(ctx, clientset, pod)
+
+	pod, err = LiveMigrationReconciler.BuildahRestore(ctx, "/tmp/checkpoints/checkpoints", clientset)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	LiveMigrationReconciler.PushDockerImage("localhost/leonardopoggiani/checkpoint-images:container-"+strconv.Itoa(numContainers-1), "container-"+strconv.Itoa(numContainers-1), pod.Name)
+
+	createContainers := []v1.Container{}
+
+	if exchange == "direct" {
+		// TODO: send to the other node
+		for i := 0; i < numContainers; i++ {
+			container := v1.Container{
+				Name:            fmt.Sprintf("container-%d", i),
+				Image:           "localhost/leonardopoggiani/checkpoint-images:container-" + strconv.Itoa(i),
+				ImagePullPolicy: v1.PullPolicy("IfNotPresent"),
+			}
+
+			createContainers = append(createContainers, container)
+		}
+	} else if exchange == "triangularized" {
+		for i := 0; i < numContainers; i++ {
+			container := v1.Container{
+				Name:            fmt.Sprintf("container-%d", i),
+				Image:           "docker.io/leonardopoggiaini/checkpoint-images:container-" + strconv.Itoa(i),
+				ImagePullPolicy: v1.PullPolicy("IfNotPresent"),
+			}
+
+			createContainers = append(createContainers, container)
+		}
+	}
+
+	// Create the Pod
+	pod, err = clientset.CoreV1().Pods("default").Create(ctx, &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("test-pod-%d-containers", numContainers),
+			Labels: map[string]string{
+				"app": "test",
+			},
+		},
+		Spec: v1.PodSpec{
+			Containers: createContainers,
+		},
+	}, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	LiveMigrationReconciler.WaitForContainerReady(pod.Name, "default", "container-"+strconv.Itoa(numContainers-1), clientset)
+
+	elapsed := time.Since(start)
+	fmt.Printf("Time to checkpoint and restore %d containers: %s\n", numContainers, elapsed)
+
+	saveToDB(db, int64(numContainers), elapsed.Seconds(), exchange, "total_times")
 }
 
 func saveToDB(db *sql.DB, numContainers int64, size float64, checkpointType string, db_name string) {
@@ -492,4 +818,188 @@ func deletePodsStartingWithTest(ctx context.Context, clientset *kubernetes.Clien
 	}
 
 	return nil
+}
+
+func performance_evaluation() {
+	// Use a context to cancel the loop that checks for sourcePod
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Connect to database
+	db, err := sql.Open("sqlite3", "./db/checkpoint_data.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Create table if it doesn't exist
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS checkpoint_sizes (timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, containers INTEGER, size FLOAT, checkpoint_type STRING)")
+	if err != nil {
+		panic(err)
+	}
+
+	// Create table if it doesn't exist
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS checkpoint_times (timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, containers INTEGER, elapsed FLOAT, checkpoint_type STRING)")
+	if err != nil {
+		panic(err)
+	}
+
+	// Create table if it doesn't exist
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS restore_times (timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, containers INTEGER, elapsed FLOAT, checkpoint_type STRING)")
+	if err != nil {
+		panic(err)
+	}
+
+	// Create table if it doesn't exist
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS docker_sizes (timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, containers INTEGER, elapsed FLOAT)")
+	if err != nil {
+		panic(err)
+	}
+
+	// Create table if it doesn't exist
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS total_times (timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, containers INTEGER, elapsed FLOAT, checkpoint_type STRING)")
+	if err != nil {
+		panic(err)
+	}
+
+	// Load Kubernetes config
+	kubeconfigPath := os.Getenv("KUBECONFIG")
+	if kubeconfigPath == "" {
+		kubeconfigPath = "~/.kube/config"
+	}
+
+	kubeconfigPath = os.ExpandEnv(kubeconfigPath)
+	if _, err := os.Stat(kubeconfigPath); os.IsNotExist(err) {
+		fmt.Println("kubeconfig file not existing")
+	}
+
+	kubeconfig, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		fmt.Println("Failed to retrieve kubeconfig")
+		return
+	}
+
+	// Create Kubernetes API client
+	clientset, err := kubernetes.NewForConfig(kubeconfig)
+	if err != nil {
+		fmt.Println("failed to create Kubernetes client")
+		return
+	}
+
+	containerCounts := []int{1}
+	// 	containerCounts := []int{1, 2, 3, 5, 10}
+	repetitions := 2
+	//  repetitions := 20
+
+	fmt.Printf("############### SIZE ###############\n")
+	for i := 0; i < repetitions; i++ {
+		fmt.Printf("Repetition %d\n", i)
+
+		for _, numContainers := range containerCounts {
+			fmt.Printf("Size for %d containers\n", numContainers)
+			getCheckpointSizePipelined(ctx, clientset, numContainers, db)
+		}
+
+		deletePodsStartingWithTest(ctx, clientset)
+
+		fmt.Printf("############### CHECKPOINT TIME ###############\n")
+
+		for _, numContainers := range containerCounts {
+			fmt.Printf("Time for %d containers\n", numContainers)
+			getCheckpointTimePipelined(ctx, clientset, numContainers, db)
+		}
+
+		deletePodsStartingWithTest(ctx, clientset)
+
+	}
+
+	for i := 0; i < repetitions; i++ {
+		fmt.Printf("############### SIZE ###############\n")
+		fmt.Printf("Repetition %d\n", i)
+
+		for _, numContainers := range containerCounts {
+			fmt.Printf("Size for %d containers\n", numContainers)
+			getCheckpointSizeSequential(ctx, clientset, numContainers, db)
+		}
+
+		deletePodsStartingWithTest(ctx, clientset)
+
+		fmt.Printf("############### CHECKPOINT TIME ###############\n")
+
+		for _, numContainers := range containerCounts {
+			fmt.Printf("Time for %d containers\n", numContainers)
+			getCheckpointTimeSequential(ctx, clientset, numContainers, db)
+		}
+
+		deletePodsStartingWithTest(ctx, clientset)
+
+	}
+
+	fmt.Printf("############### RESTORE TIME ###############\n")
+
+	for i := 0; i < repetitions; i++ {
+		fmt.Printf("Repetition %d\n", i)
+
+		for _, numContainers := range containerCounts {
+			fmt.Printf("Restore time for %d containers\n", numContainers)
+			getRestoreTime(ctx, clientset, numContainers, db)
+		}
+
+		deletePodsStartingWithTest(ctx, clientset)
+	}
+
+	fmt.Printf("############### DOCKER IMAGE SIZE ###############\n")
+
+	for i := 0; i < repetitions; i++ {
+		fmt.Printf("Repetition %d\n", i)
+
+		for _, numContainers := range containerCounts {
+			fmt.Printf("Docker image size for %d containers\n", numContainers)
+			getCheckpointImageRestoreSize(ctx, clientset, numContainers, db)
+		}
+
+		deletePodsStartingWithTest(ctx, clientset)
+
+	}
+
+	fmt.Printf("############### TOTAL TIME ###############\n")
+
+	for i := 0; i < repetitions; i++ {
+		fmt.Printf("Repetition %d\n", i)
+		for _, numContainers := range containerCounts {
+			fmt.Printf("Total times for %d containers\n", numContainers)
+			getTimeDirectVsTriangularized(ctx, clientset, numContainers, db, "triangularized")
+		}
+
+		deletePodsStartingWithTest(ctx, clientset)
+
+	}
+
+	fmt.Printf("############### DIFFERENCE TIME ###############\n")
+
+	for i := 0; i < repetitions; i++ {
+		fmt.Printf("Repetition %d\n", i)
+		for _, numContainers := range containerCounts {
+			fmt.Printf("Difference times for %d containers\n", numContainers)
+			getTimeDirectVsTriangularized(ctx, clientset, numContainers, db, "direct")
+		}
+
+		deletePodsStartingWithTest(ctx, clientset)
+
+	}
+
+	deletePodsStartingWithTest(ctx, clientset)
+
+	// Command to call the Python program
+	cmd := exec.Command("python", "graphs.py")
+
+	// Run the command and capture the output
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Print the output of the Python program
+	fmt.Println(string(output))
 }
